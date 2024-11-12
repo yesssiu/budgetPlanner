2579ffd963c42a7292a2e30d7c97d98d2559fd88
yesssiu <jessica.laamanen@edu.turkuamk.fi>
2024-11-12 15:22:56 +0200

    Update main.js
    little changes to imports

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 "use strict";
 
 const port = 3000,
-http = require("http"),
-httpStatus = require("http-status-codes"),
-router = require("./router"),
-contentTypes = require("./contentTypes"),
-utils = require("./utils");
+  http = require("http"),
+  httpStatus = require("http-status-codes"),
+  router = require("./router"),
+  contentTypes = require("./contentTypes"),
+  utils = require("./utils");
 
 const express = require("express"),
   app = express(),

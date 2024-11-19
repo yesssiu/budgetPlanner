1c350fb21dbc6caefba3993cdfea2a87080daa59
Monica Siikalampi <monica.siikalampi@icloud.com>
2024-11-19 14:06:47 +0200

    Sivuihin sisältöä

--- routes/router.js
+++ routes/router.js
@@ -18,31 +18,19 @@ router.get("/login", userController.login);
 router.post("/login", userController.authenticate);
 
 // Logout route
-router.get("/user/logout", userController.logout, userController.redirectView);
+router.get('/users/logout', userController.logout, userController.redirectView);
 
 // Signup routes
 router.get("/signup", userController.signup);
 router.post("/signup", userController.validate, userController.create, userController.redirectView);
 
-// Update user information 
-router.get("/:id/update", userController.update);
+// Update user information
+router.get('/:id/update', userController.update);
+
+// FAQ route
+router.get('/faq', (req, res) => {
+    res.render('user/faq', { title: 'FAQ & Chat' });
+});
 
 module.exports = router;
 
-//Not needed for Express?
-// exports.handle = (req, res) => {
-//   try {
-//     routes[req.method][req.url](req, res);
-//   } catch (e) {
-//     res.writeHead(httpStatus.OK, contentTypes.html);
-//     utils.getFile("views/error.html", res);
-//   }
-// };
-
-// exports.get = (url, action) => {
-//   routes["GET"][url] = action;
-// };
-
-// exports.post = (url, action) => {
-//   routes["POST"][url] = action;
-// };

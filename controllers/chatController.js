50ab62094dd1f9d9d08188c40e17672a92dd763f
Ilona Kauppila <123626072+ilokau@users.noreply.github.com>
2024-11-26 19:15:01 +0200

    Added some explanation comments

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -1,27 +1,32 @@
 const Message = require("../models/message");
 
 module.exports = io => {
+    //listen to new connections
     io.on("connection", client => {
+        //fetch the 10 most recent messages from database
         Message.find({})
             .sort({ createdAt: -1 })
             .limit(10)
-            .then(messages => {
+            .then(messages => { //sending messages to client from oldest to newest
                 client.emit("load all messages", messages.reverse());
             });
         console.log("Connected to chat");
 
+        //Listen for disconnects
         client.on("disconnect", () => {
             console.log("user disconnected");
         });
 
+        //Listen for new messages
         client.on("message", data => {
             let messageAttributes = {
                 content: data.content,
                 userName: data.userName,
                 user: data.userId
             };
-            let m = new Message(messageAttributes);
-            m.save()
+            //Create a new message instance and save to database
+            let message = new Message(messageAttributes);
+            message.save()
                 .then(() => {
                     io.emit("message", messageAttributes);
                 })
--- public/js/chat.js
+++ public/js/chat.js
@@ -6,11 +6,13 @@ const chatBox = document.getElementById('chat-box');
 const chatUserName = document.getElementById('chat-user-name');
 const chatUserId = document.getElementById('chat-user-id');
 
+//Event listener for submitting chat messages
 chatForm.addEventListener('submit', (e) => {
     e.preventDefault();
     const message = chatInput.value;
     const userName = chatUserName.value;
     const userId = chatUserId.value;
+    //Send message event to the server with the message content
     socket.emit('message', {
         content: message,
         userName: userName,
@@ -19,12 +21,14 @@ chatForm.addEventListener('submit', (e) => {
     chatInput.value = '';
 });
 
+//Listen for message events
 socket.on('message', (message) => {
     const div = document.createElement('div');
     div.textContent = `${message.userName}: ${message.content}`;
     chatBox.appendChild(div);
 });
 
+//Listen for loading all messages events
 socket.on('load all messages', (data) => {
     data.forEach(message => {
         const div = document.createElement('div');
